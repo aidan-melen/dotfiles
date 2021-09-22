815ce3d1e6ccd7d148c0bcdafca0776ac535d0be
Aidan Melen <54326778+aidan-melen@users.noreply.github.com>
2021-09-22 10:23:02 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ if [ "$CODESPACES" = "true" ]; then
   git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-  echo "source /workspaces/.codespaces/.persistedshare/dotfiles/rc" >> $HOME/.zshrc
+  echo "source /workspaces/.codespaces/.persistedshare/dotfiles/.zshrc" >> $HOME/.zshrc
   sudo chsh -s /usr/bin/zsh
 fi
