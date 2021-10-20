ed4d3584d97f24aff0289db18ebeebcb5b89384a
Aidan Melen <54326778+aidan-melen@users.noreply.github.com>
2021-10-20 15:10:02 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ if [ "$CODESPACES" = "true" ]; then
   # Check /workspaces/.codespaces/.persistedshare/creation.log for possible issues.
 fi
 
-  # Customize Terminal
-  wget -O $HOME/.oh-my-zsh/custom/themes/common.zsh-theme https://raw.githubusercontent.com/jackharrisonsherlock/common/master/common.zsh-theme
-  git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-  cp -f /workspaces/.codespaces/.persistedshare/dotfiles/.zshrc $HOME/.zshrc
-  sudo chsh -s /usr/bin/zsh
+# Customize Terminal
+wget -O $HOME/.oh-my-zsh/custom/themes/common.zsh-theme https://raw.githubusercontent.com/jackharrisonsherlock/common/master/common.zsh-theme
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+cp -f /workspaces/.codespaces/.persistedshare/dotfiles/.zshrc $HOME/.zshrc
+sudo chsh -s /usr/bin/zsh
