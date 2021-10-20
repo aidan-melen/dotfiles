a6294ea4ac0cb9ffd53295d16bd333f374ec6aba
Aidan Melen <54326778+aidan-melen@users.noreply.github.com>
2021-10-20 15:10:46 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ "$CODESPACES" = "true" ]; then
-  # put hosted codespace specific install commands here
-  # Check /workspaces/.codespaces/.persistedshare/creation.log for possible issues.
-fi
-
 # Customize Terminal
 wget -O $HOME/.oh-my-zsh/custom/themes/common.zsh-theme https://raw.githubusercontent.com/jackharrisonsherlock/common/master/common.zsh-theme
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
