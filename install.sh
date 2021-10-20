89687d1a940f2a5bb14e6ddf82a63ab069ad7ec3
Aidan Melen <54326778+aidan-melen@users.noreply.github.com>
2021-10-20 15:14:56 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,15 @@
 wget -O $HOME/.oh-my-zsh/custom/themes/common.zsh-theme https://raw.githubusercontent.com/jackharrisonsherlock/common/master/common.zsh-theme
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-cp -f /workspaces/.codespaces/.persistedshare/dotfiles/.zshrc $HOME/.zshrc
+
+if [ "$CODESPACES" = "true" ]; then
+    cp -f /workspaces/.codespaces/.persistedshare/dotfiles/.zshrc $HOME/.zshrc
+    # Check /workspaces/.codespaces/.persistedshare/creation.log for possible issues.
+else 
+    cp -f ~/dotfiles/.zshrc $HOME/.zshrc
+fi
+
 sudo chsh -s /usr/bin/zsh
+
+
+
